4cfc5ae67f3868b48be2d1fe38ea656e9a092188
JackSon_tm.m <ryantokmanmokmtm@hotmail.com>
2024-09-02 14:30:08 +0800

    Update core game logic
    * Added processGameResultAndScore to get result and word score
    * Added hostCheatingGameChecking to update datas

--- core/internal/logic/gameLogic.go
+++ core/internal/logic/gameLogic.go
@@ -7,18 +7,55 @@ import (
 	"github.com/RyanTokManMokMTM/wordle-game/core/common/regex"
 	"github.com/RyanTokManMokMTM/wordle-game/core/common/utils"
 	"log"
+	"math"
 	"os"
 	"strings"
 )
 
+/*
+		FLOW:
+		IN: HELLO, output: _____ (pick either one form the final list to check)
+		- [HELLO, WORLD, QUITE, FANCY, FRESH, PANIC, CRAZY, BUGGY].
+		- HELLO -> 00000 (1st)
+		- WORLD -> _?_0_ (2st)
+		- QUITE -> ____? (4th)
+		- FANCY -> _____ (5th)
+	   	- FRESH -> __?_? (3rd)
+		- PANIC -> _____ (5th)
+	    - CRAZY -> _____ (5th)
+		- BUGGY -> _____ (5th)
+
+		Pick all word with lower score , in this case will be
+		Final list : [FANCY,PANIC,CRAZY,BUGGY]
+		lower score is the output?
+
+		INPUT: WORLD, output: _____(pick either one form the final list to check)
+		- FANCY: _____ (2rd)
+		- PANIC: _____ (2rd)
+		- CRAZY: _?___ (1st)
+		- BUGGY: _____ (2rd)
+		Final list : [FANCY,PANIC,BUGGY]
+
+		INPUT: FRESH ,output: _____(pick either one form the final list to check)
+		- FANCY: 0____
+		- PANIC: _____
+		- BUGGY: _____
+		Final list : [PANIC,BUGGY]
+
+		INPUT: CRAZY ,output: ?_?__(pick either one form the final list to check)
+		- PANIC: _?__?
+		- BUGGY: ____0
+		Final list : [PANIC]
+
+		normal playing.
+*/
+
 // GameStart start a game for that client
 func GameStart(c client.IClient) {
 	fmt.Println("Start a Wordle Game.")
-	fmt.Println("---------------------")
+	fmt.Println("------------------------------------------------------------------------------------")
 
-	c.SetGuessingWord()
-	guessingWord := c.GetGuessingWord()
-	wordCounter := make([]uint, 52)
+	candidateList := c.GetWordList()
 	totalRound := c.GetTotalRound()
 	var currentRound uint = 0
 
@@ -26,14 +63,20 @@ func GameStart(c client.IClient) {
 	   	- Array size: [a-z][A-Z] , total 52
 	    - Index calculation: according to the ascii code table, 'a'-'a'=0 ,so arr[0] ='a', arr[1]= 'b', arr[2] = 'c' ,a[26]='A', a[27]='B',etc
 	*/
-	fmt.Println("Current word: ", guessingWord)
-	for _, w := range guessingWord {
-		index := utils.LetterIndexOf(w)
-		if index < 0 {
-			log.Fatal("Guessing word must include alphabets only")
-			return
+	candidateListSize := len(candidateList)
+	candidateCounterList := make([][]uint, candidateListSize)
+
+	//handling counter of all words
+	for i := 0; i < candidateListSize; i++ {
+		candidateCounterList[i] = make([]uint, 52)
+		for _, w := range candidateList[i] {
+			index := utils.LetterIndexOf(w)
+			if index < 0 {
+				log.Fatal("Guessing word must include alphabets only")
+				return
+			}
+			candidateCounterList[i][index]++
 		}
-		wordCounter[index]++
 	}
 
 	reader := bufio.NewReader(os.Stdin)
@@ -49,46 +92,121 @@ func GameStart(c client.IClient) {
 			continue
 		}
 
-		result, isWin := guessingWordChecking(guessText, guessingWord, wordCounter)
-		if isWin {
-			fmt.Println("Configuration! You win the game!")
-			break
-		}
+		var result string
+		var isWin bool
+		candidateList, candidateCounterList, result, isWin = hostCheatingGameChecking(guessText, candidateList, candidateCounterList)
 		fmt.Printf("Round %d result: %s\n", currentRound+1, result)
-		fmt.Println("---------------------")
-		c.SetWordHistory(guessText)
+		fmt.Println("------------------------------------------------------------------------------------")
+		if isWin {
+			fmt.Println("Configuration! You successfully guess the word!")
+			fmt.Println("------------------------------------------------------------------------------------")
+			return
+		}
 		currentRound += 1
 	}
 
-	fmt.Printf("Game is over, current round guessing word is %s\n", guessingWord)
-	fmt.Printf("Your guess words: %s\n", strings.Join(c.GetWordHistory(), ","))
-	fmt.Println("---------------------")
-	c.Reset()
+	fmt.Printf("Game is over, your are failed to guess to word in round : %d\n", totalRound)
+	fmt.Println("------------------------------------------------------------------------------------")
 }
 
-func guessingWordChecking(in, guessingWord string, wordCounter []uint) (result string, isWin bool) {
-	//Due to golang Slice send as a slice addr passing into function
-	//Copy the original wordCounter to a new slice
+func hostCheatingGameChecking(input string, candidateList []string, candidateListCounter [][]uint) (updatedCandidateList []string, updatedCandidateCounterList [][]uint, finalResult string, isWin bool) {
+	candidateListSize := len(candidateList)
+	updatedCandidateList = make([]string, 0)
+	updatedCandidateCounterList = make([][]uint, 0)
+	isWin = false
+
+	//If the candidate list has only 1 word, by as normal game
+	if candidateListSize == 0 {
+		return
+	}
+	if candidateListSize == 1 {
+		result, hit, _ := processGameResultAndScore(input, candidateList[0], candidateListCounter[0])
+		updatedCandidateList = candidateList               //No need to update
+		updatedCandidateCounterList = candidateListCounter //No need to update
+		finalResult = result
+		if hit == 5 { //Hit the word!
+			isWin = true
+		}
+		return
+	}
+
+	tempCandidateList := make([]string, candidateListSize)
+	copy(tempCandidateList, candidateList)
+
+	minHit := uint(math.MaxInt)
+	minPresent := uint(math.MaxInt)
+	candidateScore := make([][]uint, candidateListSize) //To know all candidate score
+	for i := 0; i < candidateListSize; i++ {
+		candidateScore[i] = make([]uint, 2)
+	}
+
+	candidateResult := make([]string, candidateListSize) //To store all candidate result
+
+	//Calculating the result
+	for i := 0; i < candidateListSize; i++ {
+		tempCounter := make([]uint, 52)
+		copy(tempCounter, candidateListCounter[i])
+
+		result, hit, present := processGameResultAndScore(input, candidateList[i], tempCounter)
+		if minHit > hit {
+			minHit = hit
+			minPresent = uint(math.MaxInt) //minimum hit is updated, need to reset minPresent
+		}
+
+		if minHit == hit {
+			//MARK: Only current hit met minHit be able to update present
+			/*
+				For example, minHit: 2, minPresent: 1
+				local hit = 3, present = 0
+
+				Because of 0 < 1,
+				If minPresent is always updated if present greater than minPresent, it will cause an unexpected result.
+				Final result become(minHit:2 , minPresent:0) ,which result is unexpected.
+				Expected result : minHit:2 , minPresent:1
+			*/
+			if minPresent > present {
+				minPresent = present
+			}
+		}
+
+		candidateResult[i] = result
+		candidateScore[i][0] = hit
+		candidateScore[i][1] = present
+	}
+
+	//TODO: We now know all score of candidates and the minScore
+	//TODO: We need to keep all candidates with minScore, otherwise drop it from the list
+	for i := 0; i < candidateListSize; i++ {
+		//If current candidate scope is larger than min, drop it
+		if candidateScore[i][0] > minHit { //According to the rule `More Hit will have higher scores.`
+			continue
+		} else if candidateScore[i][0] == minHit {
+			//According to the rule `If the number of Hit is the same, more Present will have higher score.`
+			if candidateScore[i][1] > minPresent {
+				continue
+			}
+		}
+
+		//Other case will be lowest candidate
+		updatedCandidateList = append(updatedCandidateList, candidateList[i]) //this list will have the same score
+		finalResult = candidateResult[i]
+
+		//Update counter list
+		updatedCandidateCounterList = append(updatedCandidateCounterList, candidateListCounter[i])
+	}
+
+	return
+}
+
+func processGameResultAndScore(in, candidate string, wordCounter []uint) (result string, hit, present uint) {
 	counter := make([]uint, len(wordCounter))
 	copy(counter, wordCounter)
 
-	size := len(guessingWord)
+	size := len(candidate)
 	temp := []byte("_____")
-	isWin = false
+	hit = 0
+	present = 0
 
-	/*
-		Explain:
-		If guessing word is "WORLD", user input is "HELLO"
-		According to the algorithm which is using a counter.
-		"WORLD" has only 1 letter 'L', "HELLO" have 2 letter 'L'
-		The expected answers will be '___0?'
-
-		If we're using 1 loop to handle this case, the answer is unexpected, which is '__?_?'
-
-		To solve this issue:
-		1. handling the correct letter and correct spot cases ,and updating the counter list
-		2. handling the other case
-	*/
 	//TODO: handling the correct letter and correct spot and updating the counter list
 	for i := 0; i < size; i++ {
 		index := utils.LetterIndexOf(rune(in[i]))
@@ -96,25 +214,24 @@ func guessingWordChecking(in, guessingWord string, wordCounter []uint) (result s
 			continue
 		}
 
-		if in[i] == guessingWord[i] {
+		if in[i] == candidate[i] {
 			counter[index] -= 1
 			temp[i] = '0'
+			hit += 1 //1 HIT
 		}
 
 	}
 
 	//TODO: Handling the other case
-	for i := 0; i < len(guessingWord); i++ {
+	for i := 0; i < size; i++ {
 		index := utils.LetterIndexOf(rune(in[i]))
 		if counter[index] == 0 {
 			continue
 		}
 		temp[i] = '?'
+		present += 1 //1 PRESENT
 	}
 
 	result = string(temp)
-	if strings.Compare(result, "00000") == 0 {
-		isWin = true
-	}
 	return
 }
