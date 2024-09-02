ae186e5b276f5ec288e4e6ec05b748bd3607b574
JackSon_tm.m <ryantokmanmokmtm@hotmail.com>
2024-09-02 14:26:55 +0800

    Updated Client
    * Removed unnecessary function
    * Updated Client interface

--- core/client/IClient.go
+++ core/client/IClient.go
@@ -1,13 +1,6 @@
 package client
 
 type IClient interface {
-	SetWordHistory(string)
-	SetGuessingWord()
-
 	GetTotalRound() uint
 	GetWordList() []string
-	GetGuessingWord() string
-	GetWordHistory() []string
-
-	Reset()
 }
--- core/client/client.go
+++ core/client/client.go
@@ -2,48 +2,18 @@ package client
 
 import (
 	"github.com/RyanTokManMokMTM/wordle-game/core/internal/config"
-	"log"
-	"math/rand"
 )
 
 type Client struct {
-	totalRound   uint
-	wordList     []string
-	guessingWord string
-	wordHistory  []string
+	totalRound uint
+	wordList   []string
 }
 
 func NewClient(c config.Config) IClient {
 	return &Client{
-		totalRound:  c.Round,
-		wordList:    c.WordList,
-		wordHistory: make([]string, 0),
+		totalRound: c.Round,
+		wordList:   c.WordList,
 	}
-}
-
-func (c *Client) SetWordHistory(w string) {
-	c.wordHistory = append(c.wordHistory, w)
-}
-
-func (c *Client) SetGuessingWord() {
-	maxLen := len(c.GetWordList()) - 1
-	if maxLen < 0 {
-		log.Fatal("word list is empty")
-	}
-
-	if maxLen == 0 {
-		c.guessingWord = c.wordList[0]
-		return
-	}
-
-	rand.NewSource(0)
-	index := rand.Intn(maxLen)
-	c.guessingWord = c.wordList[index]
-}
-
-func (c *Client) Reset() {
-	c.guessingWord = ""  //Remove guessing word
-	clear(c.wordHistory) //Reset Word History
 }
 
 func (c *Client) GetTotalRound() uint {
@@ -51,10 +21,4 @@ func (c *Client) GetTotalRound() uint {
 }
 func (c *Client) GetWordList() []string {
 	return c.wordList
-}
-func (c *Client) GetGuessingWord() string {
-	return c.guessingWord
-}
-func (c *Client) GetWordHistory() []string {
-	return c.wordHistory
 }
